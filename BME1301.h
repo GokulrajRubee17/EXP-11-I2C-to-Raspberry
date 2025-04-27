2d2407b8b993c8ad6d9a489bf1688d51d3b92c96
GokulrajRubee17 <gokulrajr@student.tce.edu>
2025-04-27 14:39:58 +0530

    Create BME1301.h

---
+++ BME1301.h
@@ -0,0 +1,19 @@
+
+#ifndef BME1301_H
+#define BME1301_H
+
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+
+#define BME1301_ADDR 0x76  // 0x77 if SDO is high
+
+typedef struct {
+    float temp;
+    float press;
+    float hum;
+} bme1301data;
+
+void bme1301init(i2c_inst_t *i2c);
+bme1301data bme1301read(i2c_inst_t *i2c);
+
+#endif
